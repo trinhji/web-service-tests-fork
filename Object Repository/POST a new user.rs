a4c33b8a6ba969a3aa9fc294f08379fd99e86d96
trinhji <boitrinhhuynh99@gmail.com>
2022-09-15 02:56:25 +0700

    test cantaloupe

--- Object Repository/POST a new user.rs
+++ Object Repository/POST a new user.rs
@@ -6,7 +6,7 @@
    <elementGuidId>bf95ec44-8f74-4b67-854c-83a7e77af5f2</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
-   <connectionTimeout>-1</connectionTimeout>
+   <connectionTimeout>0</connectionTimeout>
    <followRedirects>false</followRedirects>
    <httpBody></httpBody>
    <httpBodyContent>{
@@ -23,7 +23,7 @@
       <value>application/json</value>
       <webElementGuid>87a25015-8406-4ba8-9577-93f62dc454fb</webElementGuid>
    </httpHeaderProperties>
-   <maxResponseSize>-1</maxResponseSize>
+   <maxResponseSize>0</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
    <restRequestMethod>POST</restRequestMethod>
    <restUrl>${GlobalVariable.baseUrl}/api/users/json</restUrl>
@@ -33,7 +33,7 @@
    <soapRequestMethod></soapRequestMethod>
    <soapServiceEndpoint></soapServiceEndpoint>
    <soapServiceFunction></soapServiceFunction>
-   <socketTimeout>-1</socketTimeout>
+   <socketTimeout>0</socketTimeout>
    <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
    <variables>
       <defaultValue>18</defaultValue>
@@ -43,21 +43,21 @@
       <name>age</name>
    </variables>
    <variables>
-      <defaultValue>'FEMALE'</defaultValue>
+      <defaultValue>'UNKNOWN'</defaultValue>
       <description></description>
       <id>7c77cd5e-e69d-4bf6-bf4c-242ca104081c</id>
       <masked>false</masked>
       <name>gender</name>
    </variables>
    <variables>
-      <defaultValue>'monkey'</defaultValue>
+      <defaultValue>'cantaloupe'</defaultValue>
       <description></description>
       <id>30565e15-eece-49b1-8159-ac9a99a956aa</id>
       <masked>false</masked>
       <name>username</name>
    </variables>
    <variables>
-      <defaultValue>'iykyk'</defaultValue>
+      <defaultValue>'smoothie'</defaultValue>
       <description></description>
       <id>303e8db4-748d-4300-878d-05ef1956d9b6</id>
       <masked>false</masked>
